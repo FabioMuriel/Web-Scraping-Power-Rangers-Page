aab8b48ed49fa6ff46a1c6ceb8ae6a97f114e337
Fabio Muriel <fabiomurielvega16@gmail.com>
2024-07-26 01:24:37 -0500

    chore: Actualizar ruta de la carpeta de imágenes

--- Powerranges.py
+++ Powerranges.py
@@ -12,7 +12,7 @@ generacion_esp = []
 nombre_generaciones = []
 
 # Carpeta donde se guardaran las imagenes
-carpeta_fotos = r"C:\Users\RYZEN\Documents\Fabio\Web Scraping\src\images"
+carpeta_fotos = r"C:\Users\RYZEN\Desktop\Web-Scraping-Power-Rangers-Page\images"
 
 headers = {
     "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/81.0.4044.138 Safari/537.36",
@@ -33,7 +33,6 @@ for gen in soup.findAll("li", {"class": "leaf"}):
             generaciones.append(base_url + link_url)
             nombre_generaciones.append(link_url)
 
-
 x = 0
 y = 0
 for generacion in generaciones:
@@ -44,24 +43,23 @@ for generacion in generaciones:
     nombre_gen = nombre_generaciones[x].split("/")
     nombre_gen = nombre_gen[2]
     for enlace_individual in post.findAll("li", {"class": "leaf"}):
-        if enlace_individual.find("a") is None:
+        if enlace_individual.find("a") is None or nombre_gen == "2023_cosmicfury" or nombre_gen == "2021_dinofury" or nombre_gen == "movies":
             break
         else:
-            print("Enlace individual: ", enlace_individual)
+            # print("Enlace individual: ", enlace_individual)
             url_pr = enlace_individual.find("a").get("href")
             if enlace_individual.find("a") is None:
                 print("Enlace individual: ", enlace_individual)
 
-            print(url_pr)
-            if not ("rangers.htm" in url_pr):
+            # print(url_pr)
+            
+            if url_pr is not None and not ("rangers.htm" in url_pr):
 
                 if enlace_individual.find("a") is None:
                     print("Enlace individual: ", enlace_individual)
                     break
 
-                pr_ur_completa = (
-                    "http://www.rangercentral.com/database/" + nombre_gen + "/" + url_pr
-                )
+                pr_ur_completa = ("http://www.rangercentral.com/database/" + nombre_gen + "/" + url_pr)
                 pagina_pr = requests.get(pr_ur_completa, headers=headers).content
                 contenido = b(pagina_pr, "lxml")
 
@@ -70,26 +68,24 @@ for generacion in generaciones:
                 url_image = contenido.find("img")["src"]
 
                 ##Obtener informacin del ranger
-                informacion_ranger = informacion.find(
-                    "table", {"class": "profile-page"}
-                )
-                nombre_ranger = informacion_ranger.find("td", {"class": "profile"}).text
-                nombre_ranger = nombre_ranger.split()
-                if len(nombre_ranger) > 3:
-                    nombre_ranger = nombre_ranger[2] + " " + nombre_ranger[3]
+                informacion_ranger = informacion.find("table", {"class": "profile-page"})
+                # print(informacion_ranger)
+                
+                if informacion_ranger is not None:
+                    nombre_ranger = informacion_ranger.find("td", {"class": "profile"}).text
+                    
+                print(nombre_ranger.split(":")[1])
+                
+                if "Full Name" in nombre_ranger:
+                    nombre_ranger = nombre_ranger.split(":")[1]
                 else:
-                    nombre_ranger = nombre_ranger[2]
-                # print(nombre_ranger)
-
+                    nombre_ranger = nombre_ranger[1]
+                
                 # Obtencion de la imagen
                 pr_content_img = informacion.find("div", {"class": "profile_img"})
                 pr_image_link = pr_content_img.find("img")["src"]
-                pr_image_url_dowload = (
-                    "http://www.rangercentral.com/database/"
-                    + nombre_gen
-                    + "/"
-                    + pr_image_link
-                )
+                pr_image_url_dowload = ("http://www.rangercentral.com/database/" + nombre_gen + "/" + pr_image_link)
+                # print(pr_image_url_dowload)
 
                 ##Descarga de la imagen
                 r = requests.get(pr_image_url_dowload)
@@ -100,5 +96,10 @@ for generacion in generaciones:
                 nombre_carpeta = nombre_gen  # Puedes cambiar esto según tu necesidad
                 ruta_carpeta = os.path.join(carpeta_fotos, nombre_carpeta)
                 os.makedirs(ruta_carpeta, exist_ok=True)
+                
+                nombre_ranger = nombre_ranger.replace('"', "")
+                
+                # Guardar la imagen
+                img.save(os.path.join(ruta_carpeta, f"{nombre_ranger}.jpg"))
 
     x += 1
